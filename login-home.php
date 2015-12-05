75631e2446f30e842cfab2d6fe5c36e59d5f77c6
Victor <Vireak93@gmail.com>
2015-12-05 00:18:02 -0800

    made employee schedule link

--- login-home.php
+++ login-home.php
@@ -1,31 +1,42 @@
-<?PHP
-require_once("./include/membersite_config.php");
+	<?PHP
+	require_once("./include/membersite_config.php");
 
-if(!$fgmembersite->CheckLogin())
-{
-    $fgmembersite->RedirectToURL("login.php");
-    exit;
-}
+	if(!$fgmembersite->CheckLogin())
+	{
+		$fgmembersite->RedirectToURL("login.php");
+		exit;
+	}
 
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
-<head>
-      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-      <title>Home page</title>
-      <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css">
-</head>
-<body>
-<div id='fg_membersite_content'>
-<h2>Home Page</h2>
-Welcome back <?= $fgmembersite->UserFullName(); ?>!
+	?>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+	<head>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+		
+
+		<link rel="STYLESHEET" type="text/css" href="style/statictoolbar.css">
+	</head>
+	<body>
+		<div class="nav">
+			<ul>
+				<li><a href='access-controlled.php'>Home</a></li>
+				<li><a href='register.php'>Register</a></li>
+				<li><a href='confirmreg.php'>Confirm Register</a></li>
+				<li><a href='login.php'>Login</a></li>
+				<li><a href='employeeschedule.html'>Employee Schedule</a></li>
+
+			</ul>
+		</div>
+			<div id='fg_membersite_content'>
+			
+				Welcome back <?= $fgmembersite->UserFullName(); ?>!
+				
+
+				<p><a href='change-pwd.php'>Change password</a></p>
 <iframe src="https://calendar.google.com/calendar/embed?src=cs532group%40gmail.com&ctz=America/Los_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
-<p><a href='change-pwd.php'>Change password</a></p>
-
-<p><a href='access-controlled.php'>A sample 'members-only' page</a></p>
-<br><br><br>
-<p><a href='logout.php'>Logout</a></p>
-</div>
-</body>
-</html>
+				<p><a href='access-controlled.php'>A sample 'members-only' page</a></p>
+				<br><br><br>
+				<p><a href='logout.php'>Logout</a></p>
+			</div>
+		</body>
+		</html>
